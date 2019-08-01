36f59f3938606874e623bc592a000bf20c727cd3
Dmitry Golovko <dzmitry.halauko@itechart-group.com>
2019-08-01 14:50:41 +0300

    Initial Commit

---
+++ NeusFeet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeusFeet
+//
+//  Created by Dzmitry Halauko on 8/1/19.
+//  Copyright © 2019 Dzmitry Halauko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
